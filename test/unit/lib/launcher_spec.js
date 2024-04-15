871d7a824bb0472a577e3fbc9e9c744b7a928802
Steve-Mcl <sdmclaughlin@gmail.com>
2024-04-15 12:42:16 +0100

    unit test minimum health check value

--- test/unit/lib/launcher_spec.js
+++ test/unit/lib/launcher_spec.js
@@ -7,18 +7,31 @@ describe.only('Launcher', function () {
         const l = new launcher.Launcher({})
         should.exist(l)
     })
-    it('health check time has a default value', async function () {
-        const l = new launcher.Launcher({})
-        l.should.have.property('healthCheckInterval', 7499)
-    })
-    it('health check time can be set by user', async function () {
-        const l = new launcher.Launcher({})
-        sinon.stub(l, 'loadSettings').callsFake(() => {
-            l.settings = {
-                healthCheckInterval: 1234
-            }
+    describe('health check', function () {
+        it('has a default value', async function () {
+            const l = new launcher.Launcher({})
+            l.should.have.property('healthCheckInterval', 7499)
         })
-        await l.loadSettings()
-        l.should.have.property('healthCheckInterval', 1234)
+        it('can be set by user', async function () {
+            const l = new launcher.Launcher({})
+            sinon.stub(l, 'loadSettings').callsFake(() => {
+                l.settings = {
+                    healthCheckInterval: 1234
+                }
+            })
+            await l.loadSettings()
+            l.should.have.property('healthCheckInterval', 1234)
+        })
+        it('cannot be less than 1 second', async function () {
+            const l = new launcher.Launcher({})
+            sinon.stub(l, 'loadSettings').callsFake(() => {
+                l.settings = {
+                    healthCheckInterval: 999
+                }
+            })
+            await l.loadSettings()
+            // returns the default value when the user sets the value out of range
+            l.should.have.property('healthCheckInterval', 7499)
+        })
     })
 })
