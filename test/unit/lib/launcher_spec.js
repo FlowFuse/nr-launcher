ca2b68bc8f1c0d103d30b0a2cc38440274491e0c
Steve-Mcl <sdmclaughlin@gmail.com>
2024-12-16 12:22:24 +0000

    Add unit tests for disableAutoSafeMode feature in Launcher class

--- test/unit/lib/launcher_spec.js
+++ test/unit/lib/launcher_spec.js
@@ -1,11 +1,16 @@
 const should = require('should') // eslint-disable-line
 const sinon = require('sinon')
+const got = require('got')
+const fs = require('fs')
 const launcher = require('../../../lib/launcher.js')
 
 describe('Launcher', function () {
     it('should create a new launcher', async function () {
         const l = new launcher.Launcher({})
         should.exist(l)
+    })
+    this.afterEach(function () {
+        sinon.restore()
     })
     describe('health check', function () {
         it('has a default value', async function () {
@@ -32,6 +37,36 @@ describe('Launcher', function () {
             await l.loadSettings()
             // returns the default value when the user sets the value out of range
             l.should.have.property('healthCheckInterval', 7499)
+        })
+    })
+
+    describe('disable auto safe mode', function () {
+        it('has a default value', async function () {
+            const l = new launcher.Launcher({})
+            l.should.have.property('disableAutoSafeMode', false)
+        })
+        it('can be set by user', async function () {
+            const l = new launcher.Launcher({})
+            sinon.stub(fs, 'writeFileSync').callsFake(() => {})
+            sinon.stub(l, 'updatePackage').callsFake(() => {})
+            sinon.stub(got, 'get').callsFake(() => {
+                return {
+                    json () {
+                        return {
+                            forgeURL: 'http://localhost:1880',
+                            rootDir: '/path/to/node-red',
+                            userDir: '/path/to/.node-red',
+                            settings: {
+                                palette: {}
+                            },
+                            disableAutoSafeMode: true
+                        }
+                    }
+                }
+            })
+
+            await l.loadSettings()
+            l.should.have.property('disableAutoSafeMode', true)
         })
     })
 })
